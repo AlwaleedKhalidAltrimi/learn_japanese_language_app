867131c2fd9513cfcad51a3dba3aa5d78289e508
Alwaleed Kalid <157881255+AlwaleedKhalidAltrimi@users.noreply.github.com>
2026-03-12 11:52:29 +0300

    remove android folder and update gradle configs

--- android/app/src/main/kotlin/com/example/learn_japanese_language_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/learn_japanese_language_app/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.learn_japanese_language_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
